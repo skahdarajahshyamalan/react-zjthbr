f001dfdb6376b445761caa821b380ba0c99136a7
skahdarajahshyamalan <shyamalan1986@gmail.com>
2020-09-15 14:31:09 +0530

    test render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+//import App from "./App";
+const App =() =>{
+  return(<div>hi</div>);
+}
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
